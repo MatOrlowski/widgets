7a46791e138ce6f5ab6216ecfb65582dc487b6fe
Mateusz Orłowski <mateusz0799@gmail.com>
2021-08-04 12:59:34 +0200

    Added semantic-ui cdn and functionality to Accordion component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 
+//array of Q&A for accordion
 const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a JavaScript frontend library.'
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favourite JS library among engineers.'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components.'
-  },
-]
+	{
+		title: 'What is React?',
+		content: 'React is a JavaScript frontend library.',
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favourite JS library among engineers.',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components.',
+	},
+];
 
 export default () => {
 	return (
-		<div>
-			<Accordion items={items}/>
+		<div className="ui segment">
+			<Accordion items={items} />
 		</div>
 	);
 };
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-	return <h1>{items.length}</h1>;
+	//destructuring props into items
+	const [activeIndex, setActiveIndex] = useState(null);
+
+	const onTitleClick = (index) => {
+		setActiveIndex(index);
+	};
+
+	const singleItem = items.map((item, index) => {
+		const active = index === activeIndex ? 'active' : '';
+
+		return (
+			<React.Fragment key={item.title}>
+				<div className={`title` + active} onClick={() => onTitleClick(index)}>
+					<i className="dropdown icon"></i>
+					<strong>{item.title}</strong>
+				</div>
+				<div className={`content` + active}>
+					<p>{item.content}</p>
+				</div>
+			</React.Fragment>
+		);
+	});
+
+	return <div className="ui styled accordion">{singleItem}</div>;
 };
 
 export default Accordion;
