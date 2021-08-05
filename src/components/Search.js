8a0a649e9e039f0c676c3f2f05a5ebdb551b55fe
Mateusz Orłowski <mateusz0799@gmail.com>
2021-08-05 14:11:41 +0200

    Added axios dependency and search logic to Search.js.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  return <h1>Search</h1>
-}
+	const [term, setTerm] = useState('');
+	const [results, setResults] = useState([]);
+
+	useEffect(() => {
+		const searchWiki = async () => {
+			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query',
+					list: 'search',
+					origin: '*',
+					format: 'json',
+					srsearch: term,
+				},
+			});
+			setResults(data.query.search);
+		};
+		if (term) {
+			searchWiki();
+		}
+	}, [term]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className="item">
+				<div className="content">
+					<a
+						href={`http://en.wikipedia.org?curid=${result.pageid}`}
+						className="header"
+					>
+						{result.title}
+					</a>
+					<div className="description">
+						<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+					</div>
+				</div>
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter search term</label>
+					<div className="ui input">
+						<input
+							value={term}
+							onChange={(e) => setTerm(e.target.value)}
+							type="text"
+							className="input"
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="ui list">
+				<div>{renderedResults}</div>
+			</div>
+		</div>
+	);
+};
 
 export default Search;
