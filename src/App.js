4b8afb89c668449e43e94b08e0da5009df3bd14c
Mateusz Orłowski <mateusz0799@gmail.com>
2021-08-06 14:03:59 +0200

    Added Dropdown.js logic. Dropdown hides on clicking outside it.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
-import Dropdown from "./components/Dropdown";
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 //array of Q&A for accordion
 const items = [
-  {
-    title: "What is React?",
-    content: "React is a JavaScript frontend library.",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favourite JS library among engineers.",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components.",
-  },
+	{
+		title: 'What is React?',
+		content: 'React is a JavaScript frontend library.',
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favourite JS library among engineers.',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components.',
+	},
 ];
 
 const options = [
-  {
-    label: "The Color Red",
-    value: "red",
-  },
-  {
-    label: "The Color Green",
-    value: "green",
-  },
-  {
-    label: "The Color Blue",
-    value: "blue",
-  },
+	{
+		label: 'The Color Red',
+		value: 'red',
+	},
+	{
+		label: 'The Color Green',
+		value: 'green',
+	},
+	{
+		label: 'The Color Blue',
+		value: 'blue',
+	},
 ];
 
 export default () => {
-  return (
-    <div className="ui segment">
-      <Dropdown options={options} />
-    </div>
-  );
+	const [selected, setSelected] = useState(options[0]);
+
+	return (
+		<div className="ui segment">
+			<Dropdown
+				options={options}
+				selected={selected}
+				onSelectedChange={setSelected}
+			/>
+		</div>
+	);
 };
